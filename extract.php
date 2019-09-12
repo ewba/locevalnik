cbce63386d3084f9646e30bf38eaa0a587d626d1
Jaka Kranjc <smgl@lynxlynx.info>
2019-09-12 23:48:31 +0200

    za kosovne in gradbene odpadke prikaži dodatne informacije

--- extract.php
+++ extract.php
@@ -52,6 +52,64 @@ function getStatsDesc($oid, $dest = -1) {
   }
 }
 
+function getSpecialDesc($file, $mode = "") {
+  global $cpname;
+
+  $desc = "";
+  while(($cline = fgetcsv($file, 1000, ",")) !== FALSE) {
+    if (strcmp(strtoupper(trim(preg_replace('/[^-A-Za-z ,)(.]*/','', $cline[0]))), strtoupper($cpname))) continue;
+
+    $nacin = $cline[2];
+    $stroski = $cline[3];
+    $pogoji = $cline[4];
+    $zc = $cline[5];
+    $naslov1 = $cline[6];
+    $casi1 = $cline[7];
+    $link = $cline[8];
+
+    $pogoji = "<span class='izjema-item'>Pogoji za brezplačno oddajo:</span> $pogoji<br>";
+    $pogoji = str_replace("m3", "m<sup>3</sup>", $pogoji);
+    if ($stroski == 0) $stroski = "Da.";
+    if ($stroski == 1) $stroski = "V omejenih količinah.";
+    if ($stroski == 2) {
+      $stroski = "Ne.";
+      $pogoji = "";
+    }
+
+    if (strpos($nacin, "center") !== FALSE) {
+      $desc = <<<EEE
+<span class='izjema-item'>Zbirni center:</span> <a href="$link" target="_blank" rel="noopener">$zc</a>, $naslov1<br>
+<span class='izjema-item'>Odpiralni čas:</span> $casi1<br>
+<span class='izjema-item'>Brezplačna oddaja?</span> $stroski<br>
+$pogoji<br>
+EEE;
+    }
+
+    if ($mode == "drugi") {
+      $zc2 = $cline[9];
+      $naslov2 = $cline[10];
+      $casi2 = $cline[11];
+      if ($zc2 != "n/a") {
+        $desc .= <<<EEE
+<span class='izjema-item'>Drugi obrat:</span> $zc2, $naslov2<br>
+<span class='izjema-item'>Odpiralni čas:</span> $casi2<br><br>
+EEE;
+      }
+    }
+
+    if (strpos($nacin, "akcij") !== FALSE) {
+      $desc .= "Vsaj enkrat letno poteka akcija odvoza. ";
+    }
+
+    if (strpos($nacin, "naročil") !== FALSE) {
+      $desc .= "Odvoz lahko tudi naročite.";
+    }
+
+    break;
+  }
+  return $desc;
+}
+
 $pname = trim($_GET["x"]); //obcina
 $ptip = trim($_GET["tip"]); //odpadek
 $cpname = preg_replace('/[^-A-Za-z ]*/','', $pname);
@@ -68,10 +126,14 @@ $komunale = file_get_contents($dataFolder . 'komunale.json');
 if ($komunale) $komunale = json_decode($komunale);
 $fmap = fopen($dataFolder.'komunale-za-obmap.csv', 'r');
 $fstev = fopen($dataFolder.'odpadki.ostevilceni.csv', 'r');
-if (! $komunale || ! $fmap || !$fstev) {
+$kosovni = fopen($dataFolder . 'kosovni.csv', 'r');
+$gradbeni = fopen($dataFolder . 'gradbeni.csv', 'r');
+if (! $komunale || ! $fmap || !$fstev || !$kosovni || !$gradbeni) {
   echo "Ne morem odpreti datoteke!! Pišite na bofh@ebm.si!";
   fclose($fmap);
   fclose($fstev);
+  fclose($kosovni);
+  fclose($gradbeni);
   exit;
 }
 
@@ -93,10 +155,12 @@ while(($cline = fgetcsv($fmap, 1000, ",")) !== FALSE) {
 // pogruntaj id odpadka
 $oid = "";
 $i = 1;
+$posebnost = "";
 while(($cline = fgetcsv($fstev, 1000, ",")) !== FALSE) {
   if (strtoupper(preg_replace('/[^-A-Za-z ,)(]*/','', trim($cline[1]))) == strtoupper($cptip)) {
 //  	 echo  "$cline[0]|$cline[1]|$i||<br>";
     $oid = $cline[0];
+    $posebnost = $cline[2];
     break;
   }
   $i=$i+1;
@@ -110,6 +174,8 @@ if (empty($ckomunala)) {
   echo json_encode(array("error"=> "$msg"));
   fclose($fmap);
   fclose($fstev);
+  fclose($kosovni);
+  fclose($gradbeni);
   exit;
 }
 
@@ -119,6 +185,8 @@ if ($izjema != 0) {
     echo json_encode(array("error"=>"$msg"));
   fclose($fmap);
   fclose($fstev);
+  fclose($kosovni);
+  fclose($gradbeni);
   exit;
 }
 
@@ -137,10 +205,21 @@ foreach($komunale as $naziv => $podatki) {
     if (empty($destinacije[$odpadek])) break;
     $kam = "<strong>Odpadek '$ptip' sodi $destinacije[$odpadek].</strong> ";
     $kam .= getStatsDesc($oid, $destinacije[$odpadek]);
-    if (!empty($opombe)) {
+    // gradbeni odpadki (posebej označeni) ali kosovni (vsi, ki jih zbirajo preko kosovnega odvoza)
+    if (!empty($posebnost) || $odpadek == 7) {
+      // gradbeni imajo prednost, če je kdo slučajno označil oboje hkrati
+      if ($posebnost == "gradbeni") {
+        $opombe = getSpecialDesc($gradbeni, "drugi");
+      } else {
+        $opombe = getSpecialDesc($kosovni);
+      }
+      $opombe = '<div class="item">' . $opombe . '</div>';
+      $kam = "$kam<div class='alert alert-info'><h3 class='alert-heading'>Informacije glede oddaje</h3>$opombe</div>";
+    } else if (!empty($opombe)) {
       $opombe = '<div class="item">' . $opombe . '</div>';
       $kam = "$kam<div class='alert alert-info'><h3 class='alert-heading'>Vrste zabojnikov / način zbiranja odpadkov</h3>$opombe</div>";
     }
+
     echo json_encode(array("name"=>"$ptip",
       "kam"=>"$kam", 
       "error"=>""));
@@ -157,5 +236,7 @@ if ($found === 0) {
 //echo "eeee: $i";
 fclose($fmap);
 fclose($fstev);
+fclose($kosovni);
+fclose($gradbeni);
 
 ?>
